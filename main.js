07c9d02bcd76cd860bbc5750ad4c3eb47e45730f
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-07-27 14:12:28 +0500

    Added CSS styles and fix random color generation logic

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ let colorPara=document.getElementById('cc1');
 colorPara.textContent="Test";
 
 document.getElementById('colorbutton').onclick=function(){
-    let randomColor=Math.floor(Math.random()*16777215).toString(16);
+    let randomColor= '#' + Math.floor(Math.random()*16777215).toString(16);
     colorPara.textContent=randomColor;
+    console.log("I am here");
+    document.body.style.backgroundColor=randomColor;
+        console.log("I am here");
+
 }
